c92bd2c054dd3286456f04d33417d09f50f53e1c
Russ Curry <recurry@outlook.com>
2016-02-13 13:45:36 -0600

    Add Angelfish.AfxSystem.X.Common
    Added an assembly to provide a library for developing
    plug-in components for the system; all of the interfaces
    that are required to be implemented by plug-in components
    are declared in this assembly, along with any interfaces
    for shared services that are passed to components by the
    applications that are hosting them.

---
+++ Source/Angelfish.AfxSystem.X.Common/Plugins/IAfxComponent.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Angelfish.AfxSystem.X.Common.Plugins
+{
+    /// <summary>
+    /// The basic interface for Angelfish plug-in operators. All operators that are
+    /// developed for the system are required to implement this interface, and to use
+    /// the associated custom attributes (defined below) to provide the corresponding
+    /// metadata that the plug-in system will use to locate and import plug-ins.
+    /// </summary>
+    public interface IAfxComponent
+    {
+        /// <summary>
+        /// The initialization method that is invoked when a plug-in operator is first
+        /// added to a workflow. The services that are provided to the component when it
+        /// is initialized will be documented externally.
+        /// </summary>
+        /// <param name="services"></param>
+        void Init(IServiceProvider services);
+
+        /// <summary>
+        /// The activation method is invoked when the workflow that a plug-in operator
+        /// belongs to is activated by the system (or the end user). This is where you
+        /// would start background threads or carry out any other activities that need
+        /// to be completed in order to start processing events.
+        /// </summary>
+        /// <param name="services"></param>
+        void Activate(IServiceProvider services);
+
+        /// <summary>
+        /// The shutdown method is invoked when the workflow that a plug-in operator
+        /// belongs to is being shutdown by the system (or the end user). This would
+        /// be where you shutdown any threads or carry out any other activities that
+        /// need to be completed in order to completely shutdown the plug-in.
+        /// </summary>
+        /// <param name="services"></param>
+        void Shutdown(IServiceProvider services);
+    }
+
+    /// <summary>
+    /// The AfxOperatorIdentityAttribute is used to decorate a plug-in operator with
+    /// a display name (for use in the Visual Designer) and a GUID that will be used
+    /// to identify the plug-in when it is added to workflow definitions.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class AfxOperatorIdentityAttribute : Attribute
+    {
+        public string Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        public string Resolver { get; private set; }
+
+        public AfxOperatorIdentityAttribute(string name, string id, string resolver)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Resolver = resolver;
+        }
+    }
+
+    /// <summary>
+    /// The AfxOperatorIncomingPortAttribute is used to decorate event handlers on
+    /// a plug-in implementation so that they can be exposed as incoming endpoints
+    /// in the Visual Designer, and accept connections from plug-in operators that
+    /// implement compatible outgoing endpoints.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class AfxOperatorIncomingPortAttribute : Attribute
+    {
+        public string Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        public string Type { get; private set; }
+
+        public AfxOperatorIncomingPortAttribute(string name, string id, string type)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Type = type;
+        }
+    }
+
+    /// <summary>
+    /// The AfxOperatorOutgoingPortAttribute is used to decorate event implementations
+    /// on plug-in operators so that they can be exposed as outgoing endpoints (ports)
+    /// in the Visual Designer, and allow for the establishment of connections between
+    /// the outgoing port and compatible incoming ports on other plug-ins.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Event)]
+    public class AfxOperatorOutgoingPortAttribute : Attribute
+    {
+        public string Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        public string Type { get; private set; }
+
+        public AfxOperatorOutgoingPortAttribute(string name, string id, string type)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Type = type;
+        }
+    }
+}
+
---
+++ Source/Angelfish.AfxSystem.X.Common/Plugins/IAfxComponentResolver.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Angelfish.AfxSystem.X.Common.Plugins
+{
+    /// <summary>
+    /// The resolver interface is used to provide for a loose coupling
+    /// between plug-in components and any GUI elements that they provide
+    /// for use within the visual designer application. This interface is
+    /// intended to be implemented in a separate assembly, along with all
+    /// the plug-in component's GUI elements, and is then associated with
+    /// components via a custom attribute on each component that ties the
+    /// implementation of the plug-in component back to its corresponding
+    /// resolver implementation.
+    /// </summary>
+    public interface IAfxComponentResolver
+    {
+        /// <summary>
+        /// Retrieves a specific named property for a specific
+        /// implementation of a plug-in component.
+        /// </summary>
+        /// <param name="id">
+        /// The identity of the plug-in component that is relevant
+        /// to the property resolution request; this should be the
+        /// identifier that was assigned to the identity attribute
+        /// on the component in the package it was loaded from.
+        /// </param>
+        /// <param name="property">
+        /// The named property that is to be resolved. This can be
+        /// any kind of object, based on the future development of
+        /// the system, and the property names and expected return
+        /// values should be documented in the developer's guide.
+        /// </param>
+        /// <returns></returns>
+        object GetProperty(Guid component, string property);
+
+    }
+
+    /// <summary>
+    /// The resolver identity attribute is used to assign a unique
+    /// identifier to an implementation of a resolver, which is then
+    /// used to correlate the resolver to its relevant components by
+    /// specifying this same identifier as the 'resolver' parameter in
+    /// the component's operator identity attribute
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class AfxResolverIdentityAttribute : Attribute
+    {
+        public string Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        public AfxResolverIdentityAttribute(string name, string id)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+}
