a35c4d743f4a8ea164ada24d19bf36654d22bdd5
Russ Curry <recurry@outlook.com>
2016-02-13 13:28:32 -0600

    Add basic service container
    Added a basic service container implementation to the
    system's common code assembly; this is mainly for use
    in passing shared services to plug-in components that
    are running in the context of a workflow being hosted
    in the designer, or on the server side of the system.

--- Source/Angelfish.AfxStudio/App.xaml.cs
+++ Source/Angelfish.AfxStudio/App.xaml.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
+using Angelfish.AfxSystem.A.Common.Services;
+
 namespace Angelfish.AfxStudio
 {
     /// <summary>
@@ -13,5 +15,31 @@ namespace Angelfish.AfxStudio
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// The application service container provides a global container
+        /// for all of the services that are shared throughout the system.
+        /// </summary>
+        private AfxServices _appServices = new AfxServices();
+
+        /// <summary>
+        /// An event handler for the WPF application's "Startup" event which
+        /// is invoked before the main window has been displayed. This is where
+        /// the application will initialize common services and settings.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void Application_Startup(object sender, StartupEventArgs args)
+        {
+            // Initialize any shared services that will need to be added to the
+            // application service container prior to the UI being loaded:
+            InitializeApplicationServices();
+        }
+
+        private void InitializeApplicationServices()
+        {
+            // Add the application service container to the application's global
+            // properties so that it can be accessed anywhere in the system:
+            Application.Current.Properties.Add("App.Services", _appServices);
+        }
     }
 }
---
+++ Source/Angelfish.AfxSystem.A.Common/Services/AfxServices.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Angelfish.AfxSystem.A.Common.Services
+{
+    public class AfxServices : IServiceProvider
+    {
+        private Dictionary<Type, object> _mapServices = new Dictionary<Type, object>();
+
+        public object GetService(Type serviceType)
+        {
+            if (serviceType != null)
+            {
+                if (_mapServices.ContainsKey(serviceType))
+                {
+                    return _mapServices[serviceType];
+                }
+                else
+                {
+                    var msg = string.Format("The service \"{0}\" has not been registered.", serviceType.FullName);
+                    throw new ArgumentOutOfRangeException(msg);
+                }
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+        public void AddService(Type serviceType, object serviceImpl)
+        {
+            if ((serviceType != null) && (serviceImpl != null))
+            {
+                if (!_mapServices.ContainsKey(serviceType))
+                {
+                    _mapServices.Add(serviceType, serviceImpl);
+                }
+                else
+                {
+                    var msg = string.Format("The service \"{0}\" has already been registered.", serviceType.FullName);
+                    throw new ArgumentOutOfRangeException(msg);
+                }
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+        }
+    }
+}
