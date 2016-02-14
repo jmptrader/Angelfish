e356d4f7f584b17aff9f0d3d78efa3f276ebf06d
Russ Curry <recurry@outlook.com>
2016-02-13 21:48:25 -0600

    Add component catalog to visual designer
    Integrated the component catalog as a shared service
    in the visual designer application; the catalog is loaded
    when the application starts and is then added to the global
    service container so that it can be access as needed from
    anywhere in the application.

--- Source/Angelfish.AfxStudio/App.xaml.cs
+++ Source/Angelfish.AfxStudio/App.xaml.cs
@@ -5,8 +5,10 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Reflection;
 
 using Angelfish.AfxSystem.A.Common.Services;
+using Angelfish.AfxSystem.A.Common.Plugins.Metadata;
 
 namespace Angelfish.AfxStudio
 {
@@ -37,6 +39,22 @@ namespace Angelfish.AfxStudio
 
         private void InitializeApplicationServices()
         {
+            // Retrieve the fully-qualified path to the current assembly:
+            var pathAsm = Assembly.GetExecutingAssembly().Location;
+
+            // Isolate the path to the application, sans the filename:
+            var pathApp = System.IO.Path.GetDirectoryName(pathAsm);
+
+            // Combine to form the fully-qualified path to the plugins:
+            var pathPlugins = System.IO.Path.Combine(pathApp, "Plugins");
+
+            // Initialize the plug-in catalog and add it's interface to the
+            // application's global service container:
+            var pluginCatalog = new AfxComponentCatalog(pathPlugins);
+
+            // Add the plug-in system to the application service container:
+            _appServices.AddService(typeof(IAfxComponentCatalog), pluginCatalog);
+
             // Add the application service container to the application's global
             // properties so that it can be accessed anywhere in the system:
             Application.Current.Properties.Add("App.Services", _appServices);
