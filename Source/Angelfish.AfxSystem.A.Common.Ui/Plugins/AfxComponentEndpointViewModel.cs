28794fa73a9b1f4f0dc29beef1d6f06b856b3d69
Russ Curry <recurry@outlook.com>
2016-02-16 14:43:10 -0600

    Add AfxComponentEndpointViewModel class
    Added a class to provide the view model for the
    incoming and outgoing endpoints that are displayed
    as part of the visual representation of components
    on the workflow design surface.

---
+++ Source/Angelfish.AfxSystem.A.Common.Ui/Plugins/AfxComponentEndpointViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+using Angelfish.AfxSystem.A.Common.Plugins;
+using Angelfish.AfxSystem.A.Common.Plugins.Metadata;
+
+namespace Angelfish.AfxSystem.A.Common.Ui.Plugins
+{
+    public class AfxComponentEndpointViewModel : INotifyPropertyChanged
+    {
+        private AfxComponentEndpoint _model { get; set; }
+
+        public Guid Id
+        {
+            get { return _model.Id; }
+        }
+
+        public string Name
+        {
+            get { return _model.Name; }
+        }
+
+        public string Type
+        {
+            get { return _model.Type; }
+        }
+
+        public object Metadata
+        {
+            get { return _model.Metadata; }
+        }
+
+        public Point Centerpoint
+        {
+            get { return _centerpoint; }
+            set { _centerpoint = value; OnPropertyChanged("Centerpoint"); }
+        }
+
+        private Point _centerpoint { get; set; }
+
+        public bool IsSelected
+        {
+
+            get { return _isSelected; }
+            set { _isSelected = value; OnPropertyChanged("IsSelected"); }
+
+        }
+
+        private bool _isSelected { get; set; }
+
+        public AfxComponentEndpointViewModel(AfxComponentEndpoint model)
+        {
+            _model = model;
+        }
+
+        private void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
