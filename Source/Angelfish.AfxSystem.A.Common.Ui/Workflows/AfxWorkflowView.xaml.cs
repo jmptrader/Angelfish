d4cc0c928d84423f40b16056845aa419d311b64f
Russ Curry <recurry@outlook.com>
2016-02-16 14:57:22 -0600

    Fix basic drag-and-drop for plug-ins
    Added code for creating instances of component
    operator views and adding them to the workflow
    design surface when the user drops an instance
    of a component template onto the workflow.

--- Source/Angelfish.AfxSystem.A.Common.Ui/Workflows/AfxWorkflowView.xaml.cs
+++ Source/Angelfish.AfxSystem.A.Common.Ui/Workflows/AfxWorkflowView.xaml.cs
@@ -13,6 +13,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Angelfish.AfxSystem.A.Common.Plugins;
+using Angelfish.AfxSystem.A.Common.Plugins.Metadata;
+
+using Angelfish.AfxSystem.A.Common.Ui.Plugins;
+
 namespace Angelfish.AfxSystem.A.Common.Ui.Workflows
 {
     /// <summary>
@@ -86,7 +91,69 @@ namespace Angelfish.AfxSystem.A.Common.Ui.Workflows
 
         private void HandleDrop_Component(Point position, Guid template)
         {
+            // Access the application's global service container:
+            var appServices = Application.Current.Properties["App.Services"]
+                as IServiceProvider;
 
+            // Access the plug-in catalog which will have already
+            // been added to the application's service container:
+            var pluginCatalog = appServices.GetService(typeof(IAfxComponentCatalog))
+                as IAfxComponentCatalog;
+
+            // Retrieve the prototype from the catalog, based on the
+            // unique identifier that was dropped onto the workflow:
+            var componentType = pluginCatalog.GetComponentTemplate(template);
+
+            // Create a new instance of a component data model, using
+            // the corresponding component prototype:
+            var componentImpl = new AfxComponent(componentType);
+
+            // Set the surface coordinates on the component instance
+            // so that they can be serialized later, when the workflow
+            // is saved to a file or deployed to the server side:
+            componentImpl.Properties.Add("Surface.X", position.X.ToString());
+            componentImpl.Properties.Add("Surface.Y", position.Y.ToString());
+
+            BitmapImage componentIcon = null;
+
+            // Attempt to retrieve the resolver for the specified component
+            // so that we can get the bitmap image associated with it:
+            var resolver = pluginCatalog.GetComponentResolver(componentType.Resolver);
+            if(resolver != null)
+            {
+                componentIcon = resolver.GetProperty(componentType.Id, "Component.Bitmap") 
+                    as BitmapImage;
+            }
+
+            // Create the corresponding visual representation of the
+            // component on the design surface:
+            Surface_CreateComponent(componentImpl, componentIcon);
         }
+
+        /// <summary>
+        /// Creates the visual representation of a single
+        /// instance of a workflow component on the design
+        /// surface, at the coordinates that are specified
+        /// in the component's properties.
+        /// </summary>
+        /// <param name="component"></param>
+        private void Surface_CreateComponent(AfxComponent component, BitmapImage icon)
+        {
+            // Create the view model for the component:
+            var componentModel = new AfxComponentOperatorViewModel(component, icon);
+
+            // Create the view, assign the view model to it:
+            var componentView = new AfxComponentOperatorView(componentModel);
+
+            var X = double.Parse(component.Properties["Surface.X"]);
+            var Y = double.Parse(component.Properties["Surface.Y"]);
+
+            componentView.Margin = new Thickness(X, Y, 0, 0);
+            componentView.VerticalAlignment = VerticalAlignment.Top;
+            componentView.HorizontalAlignment = HorizontalAlignment.Left;
+
+            _ScrollViewer_Content.Children.Add(componentView);
+        }
+
     }
 }
