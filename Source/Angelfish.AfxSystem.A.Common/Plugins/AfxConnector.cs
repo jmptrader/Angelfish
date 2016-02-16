5c14328973c3cfd59e9bf0f47daba2e071622b8f
Russ Curry <recurry@outlook.com>
2016-02-16 14:34:00 -0600

    Add AfxConnector class
    Added a class that represents a logical connection
    between two different components in a workflow.

---
+++ Source/Angelfish.AfxSystem.A.Common/Plugins/AfxConnector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Angelfish.AfxSystem.A.Common.Plugins
+{
+    public class AfxConnector
+    {
+        public Guid SourceOperator { get; private set; }
+
+        public Guid SourceEndpoint { get; private set; }
+
+        public Guid TargetOperator { get; private set; }
+
+        public Guid TargetEndpoint { get; private set; }
+
+        public AfxConnector(Guid sourceOperator, Guid sourceEndpoint, Guid TargetOperator, Guid TargetEndpoint)
+        {
+            this.SourceOperator = sourceOperator;
+            this.SourceEndpoint = sourceEndpoint;
+            this.TargetOperator = TargetOperator;
+            this.TargetEndpoint = TargetEndpoint;
+        }
+    }
+}
